31a7bd4c5378b39d73c1460e4a50c4616f3f8d1b
Harvinder rathor <harvinderr09@gmail.com>
2021-05-19 23:46:18 +0530

    Add Databinding

--- app/src/main/java/com/harvinder/mvvm/MainActivity.kt
+++ app/src/main/java/com/harvinder/mvvm/MainActivity.kt
@@ -2,10 +2,16 @@ package com.harvinder.mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.harvinder.mvvm.databinding.ActivityMainBinding
+import com.harvinder.mvvm.model.User
 
 class MainActivity : AppCompatActivity() {
+    lateinit var user:User;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        user = User("Harvinder","E2-138")
+        binding.user = user
     }
 }
---
+++ app/src/main/java/com/harvinder/mvvm/model/User.kt
@@ -0,0 +1,4 @@
+package com.harvinder.mvvm.model
+
+data class User(var name:String,val address:String) {
+}
